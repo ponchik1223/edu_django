1eda7bd58bd45f3b8408a3e15d44f9058745a09c
Даниил Серов <daniilserov@MacBook-Pro-Daniil.local>
2023-09-25 17:09:29 +0300

    add registration

--- users/views.py
+++ users/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+from users.models import User
+from users.forms import UserLoginForm, UsserRegistrationForm
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm() 
+    
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UsserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form  = UsserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
